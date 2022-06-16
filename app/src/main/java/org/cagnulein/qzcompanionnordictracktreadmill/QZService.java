89a800961ca9636d48e3028d18e4c8030705ebaf
Roberto Viola <Cagnulein@gmail.com>
2022-06-16 14:39:06 +0200

    fixing tail mistake

--- app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/QZService.java
+++ app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/QZService.java
@@ -98,14 +98,14 @@ public class QZService extends Service {
         if(file != "") {
             try {
                 Runtime rt = Runtime.getRuntime();
-                String[] cmd = {"/bin/sh", "-c", " tail -n5000 | grep -a \"Changed KPH\" " + path + file + "  | tail -n1"};
+                String[] cmd = {"/bin/sh", "-c", " tail -n5000 " + path + file + " | grep -a \"Changed KPH\" | tail -n1"};
                 Process proc = rt.exec(cmd);
                 if(!speed(proc.getInputStream())) {
                     String[] cmd2 = {"/bin/sh", "-c", " grep -a \"Changed KPH\" " + path + file + "  | tail -n1"};
                     Process proc2 = rt.exec(cmd2);
                     speed(proc2.getInputStream());
                 }
-                String[] cmdIncline = {"/bin/sh", "-c", " tail -n5000 | grep -a \"Changed Grade\" " + path + file + "  | tail -n1"};
+                String[] cmdIncline = {"/bin/sh", "-c", " tail -n5000 " + path + file + " | grep -a \"Changed Grade\" | tail -n1"};
                 Process procIncline = rt.exec(cmdIncline);
                 if(!incline(procIncline.getInputStream())) {
                     String[] cmdIncline2 = {"/bin/sh", "-c", " grep -a \"Changed Grade\" " + path + file + "  | tail -n1"};
