14474fc36d69365919b72163dfe660bebca979c3
Roberto Viola <Cagnulein@gmail.com>
2022-08-11 16:24:16 +0200

    trying to understand https://github.com/cagnulein/qdomyos-zwift/issues/877#issuecomment-1212045058

--- app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/QZService.java
+++ app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/QZService.java
@@ -101,33 +101,36 @@ public class QZService extends Service {
 
         String file = pickLatestFileFromDownloads();
         DatagramSocket socketServer = null;
-        String sysbin = "/bin/";
-        File dir = new File(sysbin);
-        File[] files = dir.listFiles();
-        if (files == null || files.length == 0) {
-            System.out.println(sysbin + " not found, trying /system/bin/ instead");
-            sysbin = "/system/bin/";
-        }
 
         if(file != "") {
             try {
+                String sysbin = "/bin/";
+                File dir = new File(sysbin);
+                File[] files = dir.listFiles();
+                if (files == null || files.length == 0) {
+                    sysbin = "/system/bin/";
+                }
+                final String sh = sysbin + "sh";
+
+                System.out.println(sh + " is using");
+
                 socket = new DatagramSocket();
                 socket.setBroadcast(true);
 
                 Runtime rt = Runtime.getRuntime();
-                String[] cmd = {sysbin + "sh", "-c", " tail -n500 " + path + file + " | grep -a \"Changed KPH\" | tail -n1"};
+                String[] cmd = {sh, "-c", " tail -n500 " + path + file + " | grep -a \"Changed KPH\" | tail -n1"};
                 Process proc = rt.exec(cmd);
                 if(!speed(proc.getInputStream())) {
-                    String[] cmd2 = {sysbin + "sh", "-c", " grep -a \"Changed KPH\" " + path + file + "  | tail -n1"};
+                    String[] cmd2 = {sh, "-c", " grep -a \"Changed KPH\" " + path + file + "  | tail -n1"};
                     Process proc2 = rt.exec(cmd2);
                     if(!speed(proc2.getInputStream())) {
                         sendBroadcast(lastSpeed);
                     }
                 }
-                String[] cmdIncline = {sysbin + "sh", "-c", " tail -n500 " + path + file + " | grep -a \"Changed Grade\" | tail -n1"};
+                String[] cmdIncline = {sh, "-c", " tail -n500 " + path + file + " | grep -a \"Changed Grade\" | tail -n1"};
                 Process procIncline = rt.exec(cmdIncline);
                 if(!incline(procIncline.getInputStream())) {
-                    String[] cmdIncline2 = {sysbin + "sh", "-c", " grep -a \"Changed Grade\" " + path + file + "  | tail -n1"};
+                    String[] cmdIncline2 = {sh, "-c", " grep -a \"Changed Grade\" " + path + file + "  | tail -n1"};
                     Process procIncline2 = rt.exec(cmdIncline2);
                     if(!incline(procIncline2.getInputStream())) {
                         sendBroadcast(lastInclination);
@@ -135,7 +138,7 @@ public class QZService extends Service {
                 }
                 if(counterTruncate++ > 1200) {
                     counterTruncate = 0;
-                    String[] cmdTruncate = {sysbin + "sh", "-c", " truncate -s0 " + path + file};
+                    String[] cmdTruncate = {sh, "-c", " truncate -s0 " + path + file};
                     Process procTruncate = rt.exec(cmdTruncate);
                     Log.d(TAG, "Truncating file...");
                 }
