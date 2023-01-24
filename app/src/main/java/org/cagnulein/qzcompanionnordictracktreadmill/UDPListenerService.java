a26389cdf315d81fe645269c92e8a1883142e9f3
Roberto Viola <Cagnulein@gmail.com>
2023-01-24 08:34:01 +0100

    Update UDPListenerService.java

--- app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/UDPListenerService.java
+++ app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/UDPListenerService.java
@@ -300,7 +300,7 @@ public class UDPListenerService extends Service {
     void startListenForUDPBroadcast() {
         UDPBroadcastThread = new Thread(new Runnable() {
             public void run() {
-				while(1) {					
+				while(true) {					
 					try {
 						InetAddress broadcastIP = getBroadcastAddress();
 						Integer port = 8003;
