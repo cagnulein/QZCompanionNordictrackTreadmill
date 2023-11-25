5bcce4134f50986aac5578960ad86a3d2669d0fb
Roberto Viola <Cagnulein@gmail.com>
2023-11-25 17:51:33 +0100

    Update grant-permissions.bat

--- InstallPackage/grant-permissions.bat
+++ InstallPackage/grant-permissions.bat
@@ -1,4 +1,18 @@
+
+:: Assumes treadmill USB Debugging is turned on 
+
 @echo off
+
+set /p TMIP="Enter treadmill IP address: "
+
+ping %TMIP%
+timeout 5
+
+adb disconnect
+adb kill-server
+adb connect %TMIP%
+adb devices -l
+timeout 5
 
 adb shell pm grant org.cagnulein.qzcompanionnordictracktreadmill android.permission.READ_LOGS
 pause
