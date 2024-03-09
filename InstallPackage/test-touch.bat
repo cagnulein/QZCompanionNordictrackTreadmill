5b36d0dda2cb0913a31278361c0a6d7a02722a7a
Roberto Viola <Cagnulein@gmail.com>
2024-03-09 04:15:33 +0100

    Update test-touch.bat

--- InstallPackage/test-touch.bat
+++ InstallPackage/test-touch.bat
@@ -1,4 +1,21 @@
 @echo off
 
+::set current directory to the location of this script
+@pushd %~dp0
+
+echo *** Make sure your treadmill is powered-up and USB Debugging is turned on ***
+
+set /p TMIP="Enter treadmill IP address: "
+
+ping %TMIP%
+timeout 5
+
+adb disconnect
+adb kill-server
+adb connect %TMIP%
+adb devices -l
+timeout 5
+
+
 adb shell input swipe 100 200 300 400 200
 pause
