1a42400ee9782e9ba516c6b4e5558cf2cbee2747
Roberto Viola <Cagnulein@gmail.com>
2024-03-11 09:23:56 +0100

    Update test-touch.bat

--- InstallPackage/test-touch.bat
+++ InstallPackage/test-touch.bat
@@ -10,12 +10,12 @@ set /p TMIP="Enter treadmill IP address: "
 ping %TMIP%
 timeout 5
 
-adb disconnect
-adb kill-server
-adb connect %TMIP%
-adb devices -l
+adb disconnect >> QZ-Companion-log.txt 2>&1
+adb kill-server >> QZ-Companion-log.txt 2>&1
+adb connect %TMIP% >> QZ-Companion-log.txt 2>&1
+adb devices -l >> QZ-Companion-log.txt
 timeout 5
 
 
-adb shell input swipe 100 200 300 400 200
+adb shell input swipe 100 200 300 400 200 >> QZ-Companion-log.txt
 pause
