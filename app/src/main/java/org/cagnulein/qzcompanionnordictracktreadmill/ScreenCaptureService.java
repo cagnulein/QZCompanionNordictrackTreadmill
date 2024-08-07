f8aa450e4192cb0ba334733895b36598a7e0d4bd
Roberto Viola <Cagnulein@gmail.com>
2024-08-07 17:28:16 +0200

    adding some logs

--- app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/ScreenCaptureService.java
+++ app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/ScreenCaptureService.java
@@ -137,7 +137,7 @@ public class ScreenCaptureService extends Service {
                         int pixelStride = planes[0].getPixelStride();
                         int rowStride = planes[0].getRowStride();
                         int rowPadding = rowStride - pixelStride * mWidth;
-                        //Log.e(TAG, "Image reviewing");
+                        Log.e(TAG, "Image reviewing");
 
                           isRunning = true;
 
@@ -169,7 +169,7 @@ public class ScreenCaptureService extends Service {
                                   public void onSuccess(Text result) {
                                           // Task completed successfully
 
-                                          //Log.e(TAG, "Image done!");
+                                          Log.e(TAG, "Image done!");
 
                                           String resultText = result.getText();
                                           lastText = resultText;
@@ -204,12 +204,13 @@ public class ScreenCaptureService extends Service {
                                   @Override
                                   public void onFailure(Exception e) {
                                           // Task failed with an exception
-                                          //Log.e(TAG, "Image fail");
+                                          Log.e(TAG, "Image error");
+                                          e.printStackTrace();
                                           isRunning = false;
                                           }
                                   });
                           } else {
-                            //Log.e(TAG, "Image ignored");
+                            Log.e(TAG, "Image ignored");
                           }
                       }
             } catch (Exception e) {
