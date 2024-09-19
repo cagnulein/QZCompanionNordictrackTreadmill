14690034bb574ce0b33db317b600c029168e02f3
Roberto Viola <Cagnulein@gmail.com>
2024-09-19 18:00:33 +0200

    Update MainActivity.java

--- app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/MainActivity.java
+++ app/src/main/java/org/cagnulein/qzcompanionnordictracktreadmill/MainActivity.java
@@ -173,6 +173,13 @@ public class MainActivity extends AppCompatActivity  implements DeviceConnection
         Date date = new Date();
         Timestamp timestamp2 = new Timestamp(date.getTime());
         appLogs = appLogs + "\n" + timestamp2 + " " + command;
+
+        byte[] bytes = appLogs.getBytes(StandardCharsets.UTF_8);
+        if (bytes.length > 1000000) {
+            int excessBytes = bytes.length - 1000000;
+            int cutIndex = new String(bytes, 0, excessBytes, StandardCharsets.UTF_8).length();
+            appLogs = appLogs.substring(cutIndex);
+        }        
     }
 
     @Override
