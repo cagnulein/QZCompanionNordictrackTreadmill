f38093e95b3f61d5fc83aba928852268545fabd0
Roberto Viola <Cagnulein@gmail.com>
2024-10-21 09:36:45 +0200

    Update adb-screenshot-mac.sh

--- InstallPackage/adb-screenshot-mac.sh
+++ InstallPackage/adb-screenshot-mac.sh
@@ -7,10 +7,27 @@ read -p "Enter treadmill IP address: " TMIP
 ping -c 4 $TMIP
 sleep 5
 
-./adb disconnect
-./adb kill-server
-./adb connect $TMIP
-./adb devices -l
+# fix for 'failed to authenticate to {IP:port}'
+{
+  ./adb disconnect
+  ./adb kill-server
+  ./adb connect "$TMIP"
+} > /dev/null 2>&1
+
+{
+  ./adb disconnect
+  ./adb kill-server
+  ./adb connect "$TMIP"
+} >> QZ-Companion-log.txt 2>&1
+
+if ./adb connect "$TMIP" | grep -q 'connected to'; then
+  echo -e "ADB connection successful.\n"
+else
+  echo "Cannot establish ADB connection."
+  exit 1
+fi
+echo >> QZ-Companion-log.txt
+./adb devices -l >> QZ-Companion-log.txt
 sleep 5
 
 SAVESTAMP=$(date +"%Y-%m-%d@%H-%M-%S" | tr -d ' ')
